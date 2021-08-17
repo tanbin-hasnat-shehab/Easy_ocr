331ce80eb35d3caf029334865165423a107d189b
tanbin-hasnat-shehab <85411927+tanbin-hasnat-shehab@users.noreply.github.com>
2021-08-17 18:21:04 +0600

    Update app.py

--- app.py
+++ app.py
@@ -8,21 +8,21 @@ def about():
         contact email - tanbinhasnat04@gmail.com
         ''')
 def main():
-    st.title("circle Detection App :sunglasses: ")
-    st.write("**Using the Haar cascade Classifiers**")
-    activities = ["Home", "About"]
-    choice = st.sidebar.selectbox("Pick something fun", activities)
-    if choice == "Home":
-        st.write("Go to the About section from the sidebar to learn more about it.")
-	image_file = st.file_uploader("Upload image", type=['jpeg', 'png', 'jpg', 'webp'])
-	reader=ocr.Reader(['en','bn'])
-	results=reader.readtext(image_file)
-	text=''
-	for result in results:
-		text+=result[1] + '\n'
-	st.write(text)
-elif choice == "About":
-	about()
+	st.title("circle Detection App :sunglasses: ")
+	st.write("**Using the Haar cascade Classifiers**")
+	activities = ["Home", "About"]
+	choice = st.sidebar.selectbox("Pick something fun", activities)
+	if choice == "Home":
+		st.write("Go to the About section from the sidebar to learn more about it.")
+		image_file = st.file_uploader("Upload image", type=['jpeg', 'png', 'jpg', 'webp'])
+		reader=ocr.Reader(['en','bn'])
+		results=reader.readtext(image_file)
+		text=''
+		for result in results:
+			text+=result[1] + '\n'
+		st.write(text)
+	elif choice == "About":
+		about()
 
 if __name__ == "__main__":
     main()

46e8fee9162c7252f92e0a3c6f8b50f1ddc6594d
tanbin-hasnat-shehab <85411927+tanbin-hasnat-shehab@users.noreply.github.com>
2021-08-17 18:03:32 +0600

    Update app.py

--- app.py
+++ app.py
@@ -2,31 +2,18 @@ import easyocr as ocr
 
 import streamlit as st
 
-
-    
-    
-
-
-
 def about():
     st.write(
         '''
         contact email - tanbinhasnat04@gmail.com
         ''')
-
-
 def main():
     st.title("circle Detection App :sunglasses: ")
     st.write("**Using the Haar cascade Classifiers**")
-
     activities = ["Home", "About"]
     choice = st.sidebar.selectbox("Pick something fun", activities)
-
     if choice == "Home":
-
         st.write("Go to the About section from the sidebar to learn more about it.")
-        
-        # You can specify more file types below if you want
         image_file = st.file_uploader("Upload image", type=['jpeg', 'png', 'jpg', 'webp'])
         print(type(image_file))
 	reader=ocr.Reader(['en','bn'])
@@ -35,16 +22,8 @@ def main():
 	for result in results:
 		text+=result[1] + '\n'
 	st.write(text)
-        
-        
-
-            
-
     elif choice == "About":
         about()
-
-
-
 
 if __name__ == "__main__":
     main()
